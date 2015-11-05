5b3339238822d36c5af474cd3be3eb19ae64878b
rebort_yang <353281855@qq.com>
2015-11-05 10:40:58 +0800

    function test now checks we can input a to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 __author__ = 'yangjiebin'
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,14 +19,30 @@ class NewVisitorTest(unittest.TestCase):
         # the title and head has a word : 'To-Do'
         self.broswer.implicitly_wait(8)
         self.assertIn('To-Do lists', self.broswer.title)
-        self.fail('finish the test!')
+        header_text = self.broswer.find_element_by_tag_name('h1').text
+        self.assertEqual('To-Do lists', header_text)
+
 
         #it call you to write one 'to-do' thing
+        inputbox = self.broswer.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         #i input 'go to HK'
+        inputbox.send_keys('go to HK')
+
         #enter,web refresh
+        inputbox.send_keys(Keys.ENTER)
+
         #web table show "1. go to HK"
+        self.broswer.implicitly_wait(5)
+
+        table = self.broswer.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: go to HK' for row in rows)
+        )
         #web still has a input text.
+        self.fail('finish the test!')
         #i input again, 'buy phone'
         #web page refresh again. there are two 'to-do'
         #the web create a URL for me.and page show me some thing about it.
