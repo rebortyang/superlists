6c37c860cace6da19b38aa8ed33c26c8c9a4f4e3
rebort_yang <353281855@qq.com>
2015-11-06 11:30:22 +0800

    deal post

--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,7 @@ __author__ = 'yangjiebin'
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+import unittest, time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         #so i open it
         self.broswer.get('http://localhost:8000')
         # the title and head has a word : 'To-Do'
-        self.broswer.implicitly_wait(8)
+        time.sleep(8)
         self.assertIn('To-Do lists', self.broswer.title)
         header_text = self.broswer.find_element_by_tag_name('h1').text
         self.assertEqual('To-Do lists', header_text)
@@ -34,22 +34,26 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         #web table show "1. go to HK"
-        self.broswer.implicitly_wait(5)
-
         table = self.broswer.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: go to HK' for row in rows),
-            'new to-do item did not appear in table'
-        )
+        self.assertIn('1: go to HK', [row.text for row in rows])
+
         #web still has a input text.
-        self.fail('finish the test!')
         #i input again, 'buy phone'
+        inputbox = self.broswer.find_element_by_id('id_new_item')
+        inputbox.send_keys('buy phone')
+        inputbox.send_keys(Keys.ENTER)
+
+
         #web page refresh again. there are two 'to-do'
         #the web create a URL for me.and page show me some thing about it.
+        table = self.broswer.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: buy phone', [row.text for row in rows])
 
         #visit the URL.find 'to-do' still in.
         #sleep.
+        self.fail('finish the test!')
 
 
 if __name__ == '__main__':
